c6654f9587040cf8841579757cee0bc6b3565d0d
leonardo15043 <lahernandez15043@gmail.com>
2021-07-29 14:51:55 -0500

    Estructura inicial

---
+++ src/main/java/com/example/octans/OctansApplication.java
@@ -0,0 +1,13 @@
+package com.example.octans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/octans/OctansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.octans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
